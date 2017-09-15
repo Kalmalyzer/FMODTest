3a9775cc88b4796744285211963622ca02d60621
Mikael Kalms <mikael@kalms.org>
2017-09-15 10:47:17 +0200

    PreExportHandler works when testing locally

--- Assets/Editor/UnityCloudBuild.cs
+++ Assets/Editor/UnityCloudBuild.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 #if !UNITY_CLOUD_BUILD
 namespace UnityEngine.CloudBuild
@@ -42,20 +44,43 @@ public class UnityCloudBuild : MonoBehaviour
 {
     /// <summary>
     /// Callback which will be invoked by UCB before the build begins
-    /// UCB runs this since we have explicitly specified 'UnityCloudBuild.PreExportHandler' as the pre-export hook in the UCB web UI for our project
+    /// UCB will run this if 'UnityCloudBuild.PreExportHandler' is specified as the pre-export hook in the UCB web UI for the project
     /// References: https://docs.unity3d.com/Manual/UnityCloudBuildManifestAsScriptableObject.html
     /// References: https://developer.cloud.unity3d.com/support/guides/manifest/
     /// References: https://docs.unity3d.com/Manual/UnityCloudBuildPreAndPostExportMethods.html
+    ///
+    /// Test-run this logic by invoking a command like "<path to unity>/Unity.exe -batchmode -quit -executeMethod UnityCloudBuild.PreExportHandler" against a clean repository
+    /// References: https://docs.unity3d.com/Manual/CommandLineArguments.html
     /// </summary>
     public static void PreExportHandler(UnityEngine.CloudBuild.BuildManifestObject buildManifest)
     {
         Debug.Log("Running UnityCloudBuild.PreExportHandler");
 
-        // Ensure that FMOD's *.bank files are copied into the StreamingAssets/ folder before the regular build process begins
+        UpdateFMODBanks();
+    }
+
+    /// <summary>
+    /// Ensure that FMOD's *.bank files are copied into the StreamingAssets/ folder before the regular build process begins
+    ///
+    /// The cache updating and bank copying is normally triggered simply by having the project open for a second in the Editor.
+    /// However, if FMODStudioCache.asset* and StreamingAssets/*.bank files are excluded from source control, then FMOD's update logic needs to be run manually
+    ///   at the start of the automated build process.
+    /// </summary>
+    public static void UpdateFMODBanks()
+    {
+        // Extract countdownTimer from FMODUnity.EventManager.
+        // FMODUnity.EventManager.UpdateCache() needs to be invoked this many times, plus once, before it will actually update the cache.
+        FieldInfo countdownTimerField = typeof(FMODUnity.EventManager).GetField("countdownTimer", BindingFlags.NonPublic | BindingFlags.Static);
+        Assert.IsNotNull(countdownTimerField);
+        int countdownTimer = (int) countdownTimerField.GetValue(null);
+
         // This is normally triggered from EventManager.Update() but in the UCB case the project is copied onto a clean machine, and building
         //  is triggered without any Editor Update() ticks being performed first
-        Debug.Log("Invoking FMODUnity.EventManager.UpdateCache");
-        FMODUnity.EventManager.UpdateCache();
+        for (int i = 0; i < countdownTimer + 1; i++)
+        {
+            Debug.Log("Invoking FMODUnity.EventManager.UpdateCache");
+            FMODUnity.EventManager.UpdateCache();
+        }
         Debug.Log("Invoking FMODUnity.EventManager.CopyToStreamingAssets");
         FMODUnity.EventManager.CopyToStreamingAssets();
     }
